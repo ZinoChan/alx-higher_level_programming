ff99fb67097aa00b7f995d2cda76ef2fc7138797
ZinoChan <devzinochan@gmail.com>
2023-05-09 11:50:25 +0100

    fix

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-s1 = str * 3
-s2 = str[:9]
-print(s1)
-print(s2)
+print(str*3)
+print(str[:9])
--- 0x00-python-hello_world/del/5-print_string.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-str = "Holberton School"
-s1 = string * 3
-s2 = string[:9]
-print(s1)
-print(s2)
