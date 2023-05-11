1204ea4029f21f48aeafb77e68a9c163b7a85e73
Zino <72712106+ZinoChan@users.noreply.github.com>
2023-05-11 21:58:44 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv
-    argv_len = len(argv) - 1
-    if argv_len > 1:
-        print(argv_len, "arguments:")
-        for idx in range(1, argv_len + 1):
-            print("{:d}: {}".format(idx, argv[idx]))
+    argv_len = len(sys.argv) - 1
+
+    if argv_len == 0:
+        print("{} arguments.".format(argv_len))
     elif argv_len == 1:
-        print(argv_len, "argument:")
-        for idx in range(1, argv_len + 1):
-            print("{:d}: {}".format(idx, argv[idx]))
-    elif argv_len == 0:
-        print(argv_len, "argumentns.")
+        print("{} argument:".format(argv_len))
+    else:
+        print("{} arguments:".format(argv_len))
+
+    if argv_len >= 1:
+        argv_len = 0
+        for arg in sys.argv:
+            if argv_len != 0:
+                print("{}: {}".format(argv_len, arg))
+            argv_len += 1
