e1390cfd150fc187f2848398efabf6e1496c8f76
ZinoChan <devzinochan@gmail.com>
2023-05-11 10:23:37 +0100

    mandatory tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argv_len = len(argv) - 1
+    if argv_len > 1:
+        print(argv_len, "arguments:")
+        for idx in range(1, argv_len + 1):
+            print("{:d}: {}".format(idx, argv[idx]))
+    elif argv_len == 1:
+        print(argv_len, "argument:")
+        for idx in range(1, argv_len + 1):
+            print("{:d}: {}".format(idx, argv[idx]))
+    elif argv_len == 0:
+        print(argv_len, "argumentns.")
