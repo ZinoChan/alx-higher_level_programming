b5b6c37e5f07ba8fc8765e73474ba3b3dc840e11
ZinoChan <devzinochan@gmail.com>
2023-05-13 12:53:37 +0100

    mandatory tasks

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    res = (tuple_a[0] if len(tuple_a) > 0 else 0) + (
+        tuple_b[0] if len(tuple_b) > 0 else 0
+    ), (tuple_a[1] if len(tuple_a) > 1 else 0) + (tuple_b[1] if len(tuple_b) > 1 else 0)
+    return res
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1,)))
+print(add_tuple(tuple_a, ()))
