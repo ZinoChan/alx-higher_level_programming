12c8369c3aa8f0ae5e903499e30a4f8db39253ba
ZinoChan <devzinochan@gmail.com>
2023-05-17 17:45:18 +0100

    clean up

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,9 +5,3 @@ def multiply_by_2(a_dictionary):
         new_dict[key] = value * 2
     return new_dict
 
-
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print(a_dictionary)
-print("--")
-print(new_dict)
