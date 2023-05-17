e6d87eaf9b3f43bac41272fed430aee134cc0f50
ZinoChan <devzinochan@gmail.com>
2023-05-17 19:10:58 +0100

    more tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from functools import reduce
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total_sum = sum(score * weight for score, weight in my_list)
+    score = sum(weight for _, weight in my_list)
+
+    return total_sum / score if score != 0 else 0
