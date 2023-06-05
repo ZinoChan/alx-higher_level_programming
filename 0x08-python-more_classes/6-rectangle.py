bbf4ba5fbd0babadff4006cf89026989d620a19c
ZinoChan <devzinochan@gmail.com>
2023-06-05 23:31:31 +0100

    refactor

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -77,4 +77,4 @@ class Rectangle:
     def __del__(self):
         """ Prints a message when an instance is deleted and decrements the number of instances """
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
