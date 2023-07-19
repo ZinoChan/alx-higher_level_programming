c74aaf5d54ab32fed1c86e89d6f0d980c3d8ad43
ZinoChan <devzinochan@gmail.com>
2023-07-19 22:00:48 +0100

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the users
-SELECT * FROM user WHERE User = 'user_0d_1';
-SELECT * FROM user WHERE User = 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
