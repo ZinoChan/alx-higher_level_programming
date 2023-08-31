a35e114044f480b82ec648fba9c3ba42b3a1451c
ZinoChan <devzinochan@gmail.com>
2023-08-31 22:14:19 +0100

    fix typo

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch him
-curl -s -X PUT -H "Origin: Schoolroot" -d "user_id=98" 0.0.0.0:5000/catch_me_3
+curl -s -X PUT -H "Origin: Schoolroot" -d "user_id=98" 0.0.0.0:5000/catch_me
