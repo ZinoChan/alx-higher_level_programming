c3570b032c9272752245caadd1a604d1ab059f0e
ZinoChan <devzinochan@gmail.com>
2023-09-26 21:45:55 +0100

    fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,19 +4,27 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const characters = JSON.parse(body).characters;
-  characters.forEach((url) => {
+function requestPromise(url) {
+  return new Promise((resolve, reject) => {
     request.get(url, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      console.log(JSON.parse(body).name);
+      if (error) reject(error);
+      else resolve(body);
     });
   });
-});
+}
+
+async function printCharacters() {
+  try {
+    const body = await requestPromise(url);
+    const characters = JSON.parse(body).characters;
+
+    for (let i = 0; i < characters.length; i++) {
+      const characterBody = await requestPromise(characters[i]);
+      console.log(JSON.parse(characterBody).name);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+printCharacters();
