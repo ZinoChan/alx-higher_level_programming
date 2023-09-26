a161f59120834f5458acbfb478a53d1f4b769147
ZinoChan <devzinochan@gmail.com>
2023-09-26 11:34:57 +0100

    init

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const person = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const filmsData = JSON.parse(body);
+  const wedgeAntillesFilms = filmsData.results.filter(film => film.characters.includes(person)).length;
+
+  console.log(wedgeAntillesFilms);
+});
